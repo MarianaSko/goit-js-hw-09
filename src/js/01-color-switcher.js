122c011ca327f7b5767b0d67adfe3ec802edcf6f
MarianaSko <maryana.skorynovych@gmail.com>
2023-11-09 17:43:37 +0200

    task1 -v1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const startBtnEl = document.querySelector('[data-start]')
+const stopBtnEl = document.querySelector('[data-stop]')
+
+stopBtnEl.disabled = true;
+
+const onStartBtnClick = () => {
+
+    startBtnEl.disabled = true;
+    stopBtnEl.disabled = false;
+
+    const onStartId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000)
+
+    stopBtnEl.addEventListener('click', () => {
+        startBtnEl.disabled = false;
+        stopBtnEl.disabled = true;
+        clearInterval(onStartId)
+    })
+}
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+startBtnEl.addEventListener('click', onStartBtnClick)
 
